231485e71c045034bad6e1c6db81d7d6456753aa
nrynjoshi <narayan@uslegalpro.com>
2023-10-23 09:43:45 +0100

    SOAP web service client initial code commit

---
+++ SimpleSOAPClientApp/src/main/java/com/narayanjoshi/simplesoapclientapp/SimpleSoapClientAppApplication.java
@@ -0,0 +1,13 @@
+package com.narayanjoshi.simplesoapclientapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSoapClientAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSoapClientAppApplication.class, args);
+    }
+
+}
---
+++ SimpleSOAPClientApp/src/test/java/com/narayanjoshi/simplesoapclientapp/SimpleSoapClientAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.narayanjoshi.simplesoapclientapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSoapClientAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
